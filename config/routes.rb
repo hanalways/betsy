fde111cea75232a4b7b749d216fc98cb269de845
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-01 14:03:54 -0700

    routes up to date

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :orders, except: [:new]
-  resources :products
-=======
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :merchants
@@ -12,5 +7,4 @@ Rails.application.routes.draw do
   post "product/:id/toggle_retire", to: "products#toggle_retire", as: "toggle_retire_product"
 
   resources :orders, except: [:new]
->>>>>>> f5cd0262171ed54d90fcc923350ccd0fcaeaa6d3
 end
