507250f172a2f5d5e5ac6db2ad79b1aaa8fedb6c
Hana <hanalways@gmail.com>
2019-05-01 09:17:11 -0700

    seeded initial merchant data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+merchant_list = [
+  ["Ada Lovelace", "adalovelace@codinggod.com"],
+  ["Steve Jobs", "stevejobs@dead.com"],
+  ["Bill Gates", "billgates@gagoglezillionare.com"],
+  ["Melinda Gates", "melindagates@gagogle"],
+  ["Mark Zuckerberg", "markzuckerberg@robotalien.net"],
+  ["Jeff Bezos", "jeffbezos@pureinsanity.com"]
+]
+
+merchant_list.each do |username, email|
+  Merchant.create(
+    username: username,
+    email: email,
+  )
+end
