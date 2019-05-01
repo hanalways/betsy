3f2d267bf5158afc08772da33bbbb97e393c4ed5
Hana <hanalways@gmail.com>
2019-05-01 10:05:40 -0700

    scaffolding code for testing OAuth

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 describe MerchantsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do 
+    it "" do
+
+    end
+  end
+
+  describe "auth callback" do 
+    it "can log in an existing user" do 
+      # Arrange
+      # Pull a user from the database
+      user = Merchant.first
+
+      # Create mock data for this user as though it hasd come from github
+      mock_auth_hash = {
+        uid: user.uid,
+        provider: user.provider,
+        info: {
+          name: user.username,
+          email: user.email,
+        }
+      }
+
+      # Tell OmniAuth to use this data for the next request 
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      # Act
+      expect {
+        get auth_callback_path("github")
+      }.wont_change "Merchant.count"
+
+      # Assert
+      expect(session[:user_id]).must_equal user.id
+      must_redirect_to root_path
+    end
+  end
 end
