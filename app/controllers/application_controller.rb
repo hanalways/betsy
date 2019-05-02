7933338fed59786a8e082db065b54a571f440741
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-02 16:16:44 -0700

    total_price in order_product, tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :set_cart
 
+  private
+
   def set_cart
     @current_order = Order.find_by(id: session[:order_id])
     if !@current_order
--- test/models/order_product_test.rb
+++ test/models/order_product_test.rb
@@ -1,9 +1,27 @@
 require "test_helper"
 
 describe OrderProduct do
-  let(:order_product) { OrderProduct.first }
+  before do
+    @order_product = order_products(:op_one)
+  end
 
   it "must be valid" do
-    value(order_product).must_be :valid?
+    value(@order_product).must_be :valid?
+  end
+
+  it "must be invalid without data" do
+    invalid_op = OrderProduct.new
+    value(invalid_op).wont_be :valid?
+  end
+
+  it "must be invalid with quantity of zero" do
+    less_than_zero = OrderProduct.new(product: Product.first, order: Order.first, quantity: 0)
+    value(less_than_zero).wont_be :valid?
+  end
+
+  describe "total_price" do
+    it "calculates the total price per item" do
+      expect(@order_product.total_price).must_equal "$4.50"
+    end
   end
 end
