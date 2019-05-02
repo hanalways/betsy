1ce74cceddf425e44fa3d07fa44b4b71fde2e105
Hana <hanalways@gmail.com>
2019-05-01 17:07:13 -0700

    new user test passing

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -19,18 +19,7 @@ describe MerchantsController do
   end 
 
   describe "auth callback" do 
-    it "can log in an existing user" do 
-      
-      # Create mock data for this user as though it hasd come from github
-      # mock_auth_hash = {
-        #   uid: user.uid,
-        #   provider: user.provider,
-        #   info: {
-          #     name: user.username,
-          #     email: user.email,
-          #   }
-          # }
-          
+    it "can log in an existing user" do   
       user = merchants(:ada)
 
       # Tell OmniAuth to use this data for the next request 
@@ -43,11 +32,26 @@ describe MerchantsController do
 
       # Assert
       expect(session[:user_id]).must_equal user.id
+      # REMINDER: change this to root_path
       must_redirect_to merchants_path
     end
 
     it "creates an account for a new user and redirects to the root route" do
+      user = Merchant.new(
+        provider: "github", 
+        uid: 200, 
+        username: "New Merchant", 
+        email: "new123@merchant.com"
+      )
 
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+
+      expect {
+        get auth_callback_path("github")
+      }.must_change "Merchant.count", +1
+
+      expect(session[:user_id]).must_equal Merchant.last.id
+      must_redirect_to merchants_path
     end
 
     it "redirects to the login route if given invalid user data" do
