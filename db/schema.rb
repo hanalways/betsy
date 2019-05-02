58e5645e45692d4ac71cf21781bced35e220a3b4
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-02 09:34:01 -0700

    trying to merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_182133) do
+ActiveRecord::Schema.define(version: 2019_05_01_212339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,4 +56,5 @@ ActiveRecord::Schema.define(version: 2019_05_01_182133) do
     t.string "image_url"
     t.boolean "retired", default: false
   end
+
 end
