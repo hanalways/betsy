c139378f3c15f48e1535aa036090437a14693b2e
Shubha Rajan <shubharajan94@gmail.com>
2019-05-03 22:55:31 -0700

    fixed tests

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -7,7 +7,7 @@ class OrderProduct < ApplicationRecord
   validates :quantity, presence: true
   validates :quantity, numericality: { greater_than: 0 }
 
-  validates :availability
+  validate :availability
 
   def total_price
     total = self.product.price * self.quantity
@@ -15,8 +15,10 @@ class OrderProduct < ApplicationRecord
     return format("$%.2f", total)
   end
 
+  private
+
   def availability
-    if quantity > Product.find(product_id).quantity
+    if product_id && quantity > Product.find(product_id).quantity
       errors.add(:quantity, "quantity is greater than amount in stock")
     end
   end
