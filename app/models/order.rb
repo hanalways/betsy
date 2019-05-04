8d2f0632cc70d86fdd615dc550ee1cd0cb3933cf
Shubha Rajan <shubharajan94@gmail.com>
2019-05-03 22:47:15 -0700

    added validator which validates number of items in order in context of checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   has_many :order_products
   has_many :products, through: :order_products
+
+  validates :order_products, length: { minimum: 1 }, on: :checkout
 end
