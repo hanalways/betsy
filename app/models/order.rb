e5ad1237f4433da19bfcf4d59bada5e195ff6a3d
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-07 14:43:19 -0700

    confrimation page done

--- app/models/order.rb
+++ app/models/order.rb
@@ -22,6 +22,8 @@ class Order < ApplicationRecord
 
   validates :cvv, presence: true, on: :checkout
 
+  validate :email_must_be_valid, :cc_must_be_valid, :cvv_must_be_valid, :expiration_must_be_valid, on: :checkout
+
   def checkout_amount
     sum = 0
     self.order_products.each do |op|
@@ -32,5 +34,32 @@ class Order < ApplicationRecord
 
   def last_four
     return self.last_four_cc.to_s[-4..-1]
+  end
+
+  private
+
+  def email_must_be_valid
+    unless email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      errors.add(:email, "must be a valid email")
+    end
+  end
+
+  def cc_must_be_valid
+    unless last_four_cc.to_s.length == 16
+      errors.add("Credit card", "must have 16 digits")
+    end
+  end
+
+  def cvv_must_be_valid
+    unless cvv.to_s.length == 3 || cvv.to_s.length == 4
+      errors.add("CVV", "must have 3 or 4 digits")
+    end
+  end
+
+  def expiration_must_be_valid
+    my_date = Date.strptime(expiration, "%m/%y")
+    unless my_date > Date.today
+      errors.add("Expiration", "cannot be in the past")
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_190940) do
+ActiveRecord::Schema.define(version: 2019_05_07_211547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 2019_05_07_190940) do
     t.string "city"
     t.string "state"
     t.integer "zip"
-    t.integer "last_four_cc"
+    t.bigint "last_four_cc"
     t.string "expiration"
     t.string "name"
     t.integer "cvv"
