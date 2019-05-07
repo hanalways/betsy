209ebde6c0c23a1badb04df46109236b5bcbf311
Hana <hanalways@gmail.com>
2019-05-07 16:50:53 -0700

    order fulfillment completed

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,7 @@ class Merchant < ApplicationRecord
 
   validates :username, presence: true, uniqueness: true 
   validates :email, presence: true, uniqueness: true 
+  validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
   def self.build_from_github(auth_hash)
     merchant = Merchant.new
@@ -12,5 +13,18 @@ class Merchant < ApplicationRecord
     merchant.email = auth_hash["info"]["email"]
 
     return merchant 
+  end
+
+  def self.orders_of_status(status, id)
+    Order.where(status: status).joins(:products).merge(Product.where(merchant_id: id))
+  end
+
+
+  def revenue_of_status(status)
+    order = orders_of_status(status)
+    sum = 0
+    orders.each do |order|
+      sum += checkout_amount
+    end
   end
 end
