e23de23c9d000bfc91a05d99e89727590c95e1db
Chantal Demissie <chantaldemissie@yahoo.com>
2019-05-08 16:12:57 -0700

    only load dotenv if in development or test environment

--- config/application.rb
+++ config/application.rb
@@ -1,21 +1,22 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-Dotenv::Railtie.load
+if ["development", "test"].include? ENV["RAILS_ENV"]
+  Dotenv::Railtie.load
+end
 
 module Betsy
   class Application < Rails::Application
-  config.generators do |g|
-    # Force new test files to be generated in the minitest-spec style
-    g.test_framework :minitest, spec: true
-    # Always use .js files, never .coffee
-    g.javascript_engine :js
-  end
+    config.generators do |g|
+      # Force new test files to be generated in the minitest-spec style
+      g.test_framework :minitest, spec: true
+      # Always use .js files, never .coffee
+      g.javascript_engine :js
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
