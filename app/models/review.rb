ee2d8b9beb4afb870258ab5fb6cf38adaa85ac00
Shubha Rajan <shubharajan94@gmail.com>
2019-05-08 15:47:43 -0700

    added some validations for reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,8 @@
 class Review < ApplicationRecord
   validates :text, presence: true
   validates :rating, presence: true
-
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { less_than: 6 }
+  validates :rating, numericality: { greater_than: 0 }
   belongs_to :product
 end
